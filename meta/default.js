6f722e03a77eb1337d6f09d49b90739bb4317a59
hesoyam <user@users-MacBook-Pro.local>
2022-07-15 18:38:11 +0700

    meta

--- meta/default.js
+++ meta/default.js
@@ -11,13 +11,13 @@ export default function DefaultMeta({ title = 'Firmnardians', description = 'Ade
 			<meta property='og:type' content='website' />
 			<meta property='og:title' content={title} />
 			<meta property='og:description' content={description} />
-			<meta property='og:image' content='/img/preview.png' />
+			<meta property='og:image' content='https://firmnardians.vercel.app/img/preview.png' />
 			<meta property='og:locale' content='id_ID' />
 
 			<meta name='twitter:card' content='summary_large_image' />
 			<meta name='twitter:title' content={title} />
 			<meta name='twitter:description' content={description} />
-			<meta name='twitter:image' content='/img/preview.png' />
+			<meta name='twitter:image' content='https://firmnardians.vercel.app/img/preview.png' />
 		</Head>
 	);
 }
