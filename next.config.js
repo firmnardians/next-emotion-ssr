82204dc7ab7d823b22d6639332dcb01ceb7b43c4
hesoyam <user@users-MacBook-Pro.local>
2022-08-15 23:34:29 +0700

    change wording

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ['pbs.twimg.com', 'wasap.netlify.app', 'folderin.com'],
+		domains: ['pbs.twimg.com', 'wasap.dev', 'folderin.com'],
 	},
 };
 
